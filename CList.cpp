b87ce4c40750e2b5911b7b12be648730029c44cd
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-03-21 13:23:50 -0400

    Update CList.cpp

--- CList.cpp
+++ CList.cpp
@@ -1,7 +1,7 @@
 //Written by Steve Nwachukwu and Nick Larkin
 #include "CList.hpp"
 
-CList::~CList()  {
+CList::~CList() {
     currentCell = head;
     Cell* temp = currentCell -> next;
     for (int i = 0; i < cellCount; i++) {
@@ -27,4 +27,31 @@ ostream& CList::print(ostream& CListOutput) {
         temp = temp -> next;
         CListOutput << currentCell->upp;
     }
+}
+
+void CList::addCell(Cell *it) {
+if (head == nullptr && tail == nullptr) {
+    head = it;
+    tail = it;
+    return;
+}
+tail -> next = it;
+tail = it;
+it -> next = head;
+}
+
+void CList::init() {
+    currentCell = head;
+}
+
+Cell* CList::next() {
+    if (head == nullptr && tail == nullptr) {
+        return nullptr;
+    }
+    currentCell = currentCell -> next;
+     return currentCell;
+}
+
+void CList::remove() {
+    
 }
