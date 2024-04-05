f25e0ed8383cd7560d892c2714229444f1d1d125
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-04-05 19:17:36 -0400

    Update dice.cpp

--- dice.cpp
+++ dice.cpp
@@ -27,3 +27,75 @@ ostream& Dice::print(ostream& diceOutput) {
     return diceOutput;
 
 } //print function for the program
+
+CantStopDice::CantStopDice():Dice(4) {}
+
+const int* CantStopDice::newRoll() {
+    int combinedPair;
+    Dice::roll();
+    cout << "Please select the most important name and dice pair for this game:\n";
+    const int *diceArray = Dice::roll();
+    cout << "Output A: " << diceArray[0];
+    cout << "\nOutput B: " << diceArray[1];
+    cout << "\nOutput C: " << diceArray[2];
+    cout << "\nOutput D: " << diceArray[3];
+    cin >> combinedPair;
+    while (true) {
+        if (combinedPair == 1 || combinedPair == 2) {
+            cout << "Selected input works!" << endl;
+            break;
+        } else {
+            cout << "Please try again!" << endl;
+            cin >> combinedPair;
+        }
+    }
+    if (combinedPair == 1) {
+        dicePair[0] = diceArray[0] + diceArray[1];
+        dicePair[1] = diceArray[2] + diceArray[3];
+    } else if (combinedPair == 2) {
+        dicePair[0] = diceArray[2] + diceArray[3];
+        dicePair[1] = diceArray[0] + diceArray[1];
+    } else {
+        cout << "Sorry but your input was not validated!" << endl;
+    }
+    return dicePair;
+}
+
+    ///////////////////////////////////////////
+
+    ///////////
+    /*FakeDice::FakeDice() : CantStopDice() {
+
+    }*/
+
+    /*FakeDice::FakeDice(const std::string& filename) : CantStopDice() {
+        inputFile.open(filename);
+        if (!inputFile.is_open()) {
+            cerr << "Error: Could not open file " << filename << std::endl;
+            fatal(); // call fatal
+        }
+    }*/
+
+    /*FakeDice::~FakeDice() {
+        if (inputFile.is_open()) {
+            inputFile.close();
+        }
+    }*/
+
+    /*void FakeDice::roll() {
+        if (!inputFile.is_open()) {
+            cerr << "Error: Input file is not open" << std::endl;
+            fatal(); // Terminate the program if file is not open
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            inputFile >> diceValues[i]; // Read four dice
+        }
+
+        // pairing values
+        pairValues[0] = diceValues[0] + diceValues[1];
+        pairValues[1] = diceValues[2] + diceValues[3];
+    }
+
+    return dicePair;
+} //this function is utilized to override dices roll function*/
